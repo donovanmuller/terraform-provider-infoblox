dc5884b8c03c1c17488ce98d15c38f01b3efc367
Robert Cannon <rob@cannonsoftware.com>
2019-06-08 09:36:09 -0400

    Powershell script to install plugin for any os

---
+++ scripts/install-plugin.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/pwsh
+
+$version = '0.2.0'
+
+$arch = if ([Environment]::Is64BitOperatingSystem) { 'amd64' } else { '386' }
+if ($IsWindows) {
+    $os = 'windows'
+    $ext = '.exe'
+    $pluginPath = Join-Path $env:APPDATA "terraform.d\plugins\$($os)_$($arch)"
+    $pluginDest = Join-Path $pluginPath "terraform-provider-infoblox_v$($version)$($ext)"
+}
+else {
+    $os = if ($IsMacOs) { 'darwin' } else { 'linux' }
+    $ext = ''
+    $pluginPath = Join-Path $env:HOME '.terraform/plugins'
+    $pluginDest = Join-Path $pluginPath "$($os)_$($arch)_terraform-provider-infoblox_v$($version)$($ext)"
+}
+$pluginSource = Join-Path './dist' "$($os)_$($arch)_terraform-provider-infoblox_v$($version)$($ext)"
+
+if (-not (Test-Path $pluginPath)) {
+    New-Item $pluginPath -ItemType Directory | Out-Null
+}
+Write-Host "Copying $pluginSource to $pluginDest"
+Copy-Item $pluginSource $pluginDest -Force
